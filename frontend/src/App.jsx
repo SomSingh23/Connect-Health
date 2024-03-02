55d4f60090e8355b0b912da5423c3e200ec60404
SomSingh23 <githubsom@gmail.com>
2024-03-02 22:19:01 +0530

    home page changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,30 @@
 import "./App.css";
+import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Navbar from "./Navbar/NavBar";
 export default function App() {
+  const [isPatient, setIsPatient] = useState(false);
+  const [isDoctor, setIsDoctor] = useState(false);
+
+  const role = useLoaderData();
+  useEffect(() => {
+    console.log(role);
+    if (role === "patient") {
+      setIsPatient(true);
+    } else if (role === "doctor") {
+      setIsDoctor(true);
+    } else {
+      setIsPatient(false);
+      setIsDoctor(false);
+    }
+  }, []);
   return (
     <>
-      <Navbar />
+      <Navbar
+        isPatient={!isPatient}
+        isDoctor={!isDoctor}
+        isLogout={isPatient || isDoctor}
+      />
       <div className="home_page"></div>
     </>
   );
