e980c6e75bfa456b21a2f357286adc4789dd0741
SomSingh23 <githubsom@gmail.com>
2024-03-04 21:08:16 +0530

    updating server

--- backend/test.js
+++ backend/test.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
-let jwt = require("jsonwebtoken");
-let token = jwt.sign({ role: "doctor" }, process.env.JWT_SECRET);
-console.log(token);
+// require("dotenv").config();
+// let jwt = require("jsonwebtoken");
+// let token = jwt.sign({ role: "doctor" }, process.env.JWT_SECRET);
+// console.log(token);
