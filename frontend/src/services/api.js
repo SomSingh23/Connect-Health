65a60722116d86d6c995d051649e96ddff340249
SomSingh23 <githubsom@gmail.com>
2024-03-13 13:09:07 +0530

    new backend domain

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,3 @@
 // const BACKEND_URL = "http://localhost:3001";
-const BACKEND_URL = "https://alpine-backend-hackiniiitp.vercel.app";
+const BACKEND_URL = "https://connectihealth-backend.vercel.app";
 export default BACKEND_URL;
