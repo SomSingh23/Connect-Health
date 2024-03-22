6c47f9e5964f64e2484f246462ca11de18cb8fd8
SomSingh23 <githubsom@gmail.com>
2024-03-23 02:35:55 +0530

    navbar look changed added logo,favicon updated

--- frontend/src/Navbar/NavBar.jsx
+++ frontend/src/Navbar/NavBar.jsx
@@ -1,20 +1,24 @@
 import { Link } from "react-router-dom";
 import "./navbar.css";
+import navbarLogo from "../../public/navbar/connect-health-removebg-preview2.png";
 export default function Navbar({ isPatient, isDoctor, isLogout }) {
   return (
     <>
-      <div className="navbar">
-        <Link to="/">Home</Link>
-        {isPatient && <Link to="/patient">Patient</Link>}
-        {isDoctor && <Link to="/doctor">Doctor</Link>}
-        {isPatient && <Link to="/ai_doctor">AI Doctor</Link>}
-        <Link to="/chat_bot">AI Assistant</Link>
+      <div className="main_navbar">
+        <img src={navbarLogo} alt="Connect Health" height={100} />
+        <div className="navbar">
+          <Link to="/">Home</Link>
+          {isPatient && <Link to="/patient">Patient</Link>}
+          {isDoctor && <Link to="/doctor">Doctor</Link>}
+          {isPatient && <Link to="/ai_doctor">AI Doctor</Link>}
+          <Link to="/chat_bot">AI Assistant</Link>
 
-        {isLogout && (
-          <Link style={{ color: "red" }} to="/logout">
-            <b>Logout</b>
-          </Link>
-        )}
+          {isLogout && (
+            <Link style={{ color: "red" }} to="/logout">
+              <b>Logout</b>
+            </Link>
+          )}
+        </div>
       </div>
     </>
   );
