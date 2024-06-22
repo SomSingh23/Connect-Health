1ed7def16df02103998bb54e58c7ec9e02f8ae75
SomSingh23 <githubsom@gmail.com>
2024-06-22 17:30:48 +0530

    something is wrong with production 2 server so switching to production 1

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
 // const BACKEND_URL = "http://localhost:3001";
-// const BACKEND_URL = "https://connectihealth-backend.vercel.app"; // production1
-const BACKEND_URL = "https://connect-health.onrender.com"; // production2
+const BACKEND_URL = "https://connectihealth-backend.vercel.app"; // production1
+// const BACKEND_URL = "https://connect-health.onrender.com"; // production2
 export default BACKEND_URL;
