e8bc0073a326077a8bffc64f7a9d79fa40704c4b
SomSingh23 <githubsom@gmail.com>
2024-08-06 15:37:59 +0530

    meta data added to index.html

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
 // const BACKEND_URL = "http://localhost:3001";
-// const BACKEND_URL = "https://connectihealth-backend.vercel.app"; // production1 state-less
-const BACKEND_URL = "https://connect-health.onrender.com"; // production2 state-full
+// const BACKEND_URL = "https://connectihealth-backend.vercel.app"; // production1 state-less + serverless
+const BACKEND_URL = "https://connect-health.onrender.com"; // production2 state-full + serverless
 export default BACKEND_URL;
