47ee2d837f33bf35fb1ae2269c75eb632f9e5335
SomSingh23 <githubsom@gmail.com>
2025-01-01 00:15:51 +0530

    happy new year

--- frontend/src/Copyright/Copyright.jsx
+++ frontend/src/Copyright/Copyright.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2024 Som Singh Lodhi. All rights reserved.</p>
+      <p>&copy; 2025 Som Singh Lodhi. All rights reserved.</p>
 
       <p>
         Website by <a href="https://github.com/somsingh23">Som</a> and{" "}
